72624a7d1e90b97c8b1dbc8bf9d6eeb6ef3e68c4
rodMike54 <michael.rodriguez.041@my.csun.edu>
2026-09-11 13:32:50 -0700

    wanted to add a new button to showcase party

--- hw0.github.io-main/script.js
+++ hw0.github.io-main/script.js
@@ -3,3 +3,17 @@
 
 console.log("Hello, world!");
 console.log("Nice to meet you! My name is Michael Rodriguez");
+
+// Find the button on the webpage
+const button = document.getElementById('confettiBtn');
+
+// Listen for a click on the button
+//super fun lol
+button.addEventListener('click', () => {
+    // This shoots random confetti from the center of the screen
+    confetti({
+        particleCount: 150, // How many pieces of confetti
+        spread: 80,         // How wide they scatter
+        origin: { y: 0.6 }  // Slightly below the middle of the screen
+    });
+});
